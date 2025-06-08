023fd8d98d3e3b5eb4dffd02011ec70fb8b8f50e
Jack Merrett <jack.m.merrett@gmail.com>
2025-06-08 10:00:38 -1000

    replacing all the print() functions with returns

--- functions.py
+++ functions.py
@@ -1,35 +1,24 @@
-# Import all the essential libraries.
-
 import random
 from lists import character_class_ls, character_race_ls, character_alignment_ls, backgrounds_ls
 
-
-# 
 def Character_Class():
-   class_random_val = random.randint(0,len(character_class_ls)-1)
-   print(f'Class: {character_class_ls[class_random_val]}')
-
+    class_random_val = random.randint(0, len(character_class_ls) - 1)
+    return character_class_ls[class_random_val]
 
 def Character_Race():
-   race_random_val = random.randint(0,len(character_race_ls)-1)
-   print(f'Race: {character_race_ls[race_random_val]}')
-
+    race_random_val = random.randint(0, len(character_race_ls) - 1)
+    return character_race_ls[race_random_val]
 
 def Character_Alignment():
-    alignment_random_val = random.randint(0,len(character_alignment_ls)-1)
-    print(f'Alignment: {character_alignment_ls[alignment_random_val]}')
-
+    alignment_random_val = random.randint(0, len(character_alignment_ls) - 1)
+    return character_alignment_ls[alignment_random_val]
 
 def Character_Background():
-    background_random_val = random.randint(0,len(backgrounds_ls)-1)
-    print(f'Background: {backgrounds_ls[background_random_val]}')
-
-
-#Point buy Radomizer: A system that starts with a Value of 8 in all stats and uses a random value to assign point from a pool of 27 point. Depending on the incread it will subtract from the pool of point at varying cost.
-#Cost: 8(-0), 9(-1), 10(-2), 11(-3), 12(-4),13(-5),14(-7),15(-9)
+    background_random_val = random.randint(0, len(backgrounds_ls) - 1)
+    return backgrounds_ls[background_random_val]
 
 def Point_Buy_Randomizer():
-    pool = 27 # This is the classic starting ammount for the "point-Buy" system.
+    pool = 27
 
     ability_scores = {
         "STR": 8,
@@ -44,9 +33,8 @@ def Point_Buy_Randomizer():
         costs = {9: 1, 10: 2, 11: 3, 12: 4, 13: 5, 14: 7, 15: 9}
         return costs.get(val, 0)
 
-    # Step 1: Assign all stats to 8 (default), then try random upgrades
     stats = list(ability_scores.keys())
-    random.shuffle(stats)  # Shuffle stat order for more variety
+    random.shuffle(stats)
 
     for stat in stats:
         while True:
@@ -57,7 +45,6 @@ def Point_Buy_Randomizer():
                 pool -= cost
                 break
 
-    # Step 2: Use leftover pool to upgrade stats, if possible
     while pool > 0:
         made_progress = False
         for stat in stats:
@@ -71,22 +58,22 @@ def Point_Buy_Randomizer():
                     made_progress = True
         if not made_progress:
             break
-   
-    print(f"\nFinal stats: {ability_scores}")
-    print(f"Remaining point(s): {pool}")
+
+    return ability_scores, pool
 
 def generate_character():
     ch_race = Character_Race()
     ch_class = Character_Class()
     ch_background = Character_Background()
     ch_alignment = Character_Alignment()
-    ch_point_buy = Point_Buy_Randomizer()
-    
+    ch_stats, ch_pool = Point_Buy_Randomizer()
 
-   return {        
+    return {
         'race': ch_race,
         'class': ch_class,
         'background': ch_background,
         'alignment': ch_alignment,
-        'stats': ch_point_buy  # whatever variable stores your stats
-   }
+        'stats': ch_stats,
+        'remaining_points': ch_pool,
+        'message': "This is a good starting point, feel free to build from here or go your own direction... that is half the fun!"
+    }
